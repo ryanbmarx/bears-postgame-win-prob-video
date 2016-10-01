d2f41923374b3ed585073955f8d348552dc002d9
ryanbmarx <ryanbmarx@gmail.com>
2016-09-30 20:11:08 -0500

    Update to reflect new css selectors.

--- js/src/title-card.js
+++ js/src/title-card.js
@@ -7,22 +7,35 @@ options = {
 	imgCredit : "xyz photo",
 	dispatch:'xyzEnd',
 	subtext:'',
-	delay:1000
+	delay:1000,
+	changeInBearsWinProb:.05
 }
 
 */
 
+
+
 function titleCard(options, callback){
 	console.log(options);
+
+	
+
 	if (options.img != undefined && options.credit != undefined){
-		d3.select('.title-card')
+		d3.select('#title-card')
 			.style('background-image', `url(./img/${window.gameID}/${options.img})`)	
 			.style('background-size', 'cover');
 		
-		d3.select('.credit')
+		d3.select('#title-card')
+			.append('p')
+			.classed('credit', true)
 			.text(options.credit);
+	} else{
+		// d3.select('.title-card')
+		// 	.style('background-image', `url(./img/${window.gameID}/${options.img})`)	
 	}
-	d3.select('.title-card .main-text')
+	d3.select('#title-card')
+		.append('p')
+		.classed('main-text', true)
 		.text(options.text)
 		.style('transform', 'translate(-3000,0)')
 		.transition()
@@ -33,19 +46,22 @@ function titleCard(options, callback){
 			.delay(options.delay * .8)
 			.style('transform', 'translate(500px,0)');
 
-	d3.select('.title-card .sub-text')
-		.text(options.subtext)
-		.style('transform', 'translate(-3000,0)')
-		.transition()
-			.duration(window.transition * 1.4)
-			.style('transform', 'translate(0,0)')
-		.transition()
-			.duration(window.transition)
-			.delay(options.delay * .7)
-			.style('transform', 'translate(500px,0)');
+		if(options.subtext){
+				d3.select('#title-card')
+				.append('p')
+				.classed('sub-text', true)
+				.text(options.subtext)
+				.style('transform', 'translate(-3000,0)')
+				.transition()
+					.duration(window.transition * 1.4)
+					.style('transform', 'translate(0,0)')
+				.transition()
+					.duration(window.transition)
+					.delay(options.delay * .7)
+					.style('transform', 'translate(500px,0)');
+				}
 
-
-	d3.select('.title-card')
+	d3.select('#title-card')
 		.style('transform', 'translate(-2000px,0)')
 		.style('opacity', 0)
 		.transition()
@@ -59,6 +75,7 @@ function titleCard(options, callback){
 			.style('transform', 'translate(2000px,0)')
 			.on('end', () => {
 				if(callback){
+					d3.selectAll('#title-card *').remove();
 					callback()
 				};
 			});
